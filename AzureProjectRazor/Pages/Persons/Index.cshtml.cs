aff1e1d6cd97b89a922c3d3bbce1ca84d6e64f99
Sai jangala <Sai_jangala-v@isb.edu>
2025-10-25 13:20:20 +0530

    Added EF Core and azure sql Db Connection

---
+++ AzureProjectRazor/Pages/Persons/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using AzureProjectRazor.Data;
+
+namespace AzureProjectRazor.Pages.Persons
+{
+    public class IndexModel : PageModel
+    {
+        private readonly AzureProjectRazor.Data.AppDbContext _context;
+
+        public IndexModel(AzureProjectRazor.Data.AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Person> Person { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Person = await _context.Persons.ToListAsync();
+        }
+    }
+}
